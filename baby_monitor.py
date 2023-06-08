0ef049030e00ea614fa2f251a28c64de33579239
David Akre <dakre21@gmail.com>
2023-06-07 22:33:04 -0600

    Adding yolo detector + tracking

--- baby_monitor.py
+++ baby_monitor.py
@@ -3,6 +3,7 @@ import Adafruit_DHT
 import threading
 import time
 import RPi.GPIO as GPIO
+import numpy as np
 
 from flask import (Flask, Response, render_template)
 
@@ -44,13 +45,50 @@ def poll_sensors(dp, rate):
         time.sleep(rate)
 
 
+def get_bbox(frame, outs, idx=0, objectness_threshold=0.5, conf_threshold=0.5, nms_threshold=0.5):
+    frame_height, frame_width = frame.shape[:2]
+
+    class_ids = []
+    boxes = []
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if class_id == idx and detection[4] > objectness_threshold and confidence > conf_threshold:
+                center_x = int(detection[0] * frame_width)
+                center_y = int(detection[1] * frame_height)
+                width = int(detection[2] * frame_width)
+                height = int(detection[3] * frame_height)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+    
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    return [] if not len(indices) else boxes[indices[0]]
+
+
 def acquire_frames():
     cap = cv2.VideoCapture(0)
+
+    net = cv2.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
+
+    tracker = cv2.TrackerCSRT_create()
+
+    layers = net.getLayerNames()
+    output_layers = [layers[i - 1] for i in net.getUnconnectedOutLayers()]
     
     dp = Datapipe()
-    poll_sensors_t = threading.Thread(target=poll_sensors, args=(dp,10,), daemon=True)
+    poll_sensors_t = threading.Thread(target=poll_sensors, args=(dp, 10,), daemon=True)
     poll_sensors_t.start()
 
+    tracking = False
     last = [0., 0., 0]
     while cap.isOpened():
         ret, frame = cap.read()
@@ -60,6 +98,32 @@ def acquire_frames():
         msg = dp.get_msg() 
         if not len(msg):
             msg = last
+
+        bbox = []
+        if not tracking:
+            blob = cv2.dnn.blobFromImage(frame, 1./255., (320, 320), [0,0,0], 1, crop=False)
+
+            net.setInput(blob)
+
+            outs = net.forward(output_layers)
+
+            bbox = get_bbox(frame, outs)
+            error_txt = 'Baby not detected'
+            if len(bbox) == 0:
+                cv2.putText(frame, error_txt, (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 1)
+            else:
+                tracker.init(frame, bbox)
+                tracking = True
+
+                ok, bbox = tracker.update(frame)
+                if not ok:
+                    cv2.putText(frame, error_txt, (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 1)
+                    tracking = False
+
+        if len(bbox) == 4:
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (0, 255, 0))
 
         brightness = 'room is bright' if not msg[2] else 'room is dark'
         txt = 'Temp: {0:0.1f}F Hummidity: {1:0.1f}%'.format(msg[1], msg[0])
@@ -76,6 +140,8 @@ def acquire_frames():
         frame = img.tobytes()
         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+    cap.release()
+
 
 @app.route('/')
 def index():
@@ -88,4 +154,4 @@ def stream():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=True)
